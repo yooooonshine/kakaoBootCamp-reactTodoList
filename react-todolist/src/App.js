4995c8d1c3d032f2bd34699fc8267f44ff249cca
hl3470 <hl3470@naver.com>
2024-07-15 17:42:03 +0900

    refactor : App.js 변경
    내부 내용들을 todoList로 넣었다.

--- react-todolist/src/App.js
+++ react-todolist/src/App.js
@@ -1,20 +1,13 @@
 import './App.css';
-import TodoInput from "./modules/todoInput/todoInput";
 import TodoList from "./modules/todoList/todoList";
 
 function App() {
-
-    function handleClickTodoInput() {
-
-    }
 
     return (
         <div className="App">
             <header>
                 <h1>TO DO LIST</h1>
             </header>
-            <TodoInput onClick={handleClickTodoInput}></TodoInput>
-
             <hr/>
             <TodoList></TodoList>
         </div>
