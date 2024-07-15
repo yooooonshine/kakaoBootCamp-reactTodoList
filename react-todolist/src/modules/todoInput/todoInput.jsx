1d521006a2eb0c8250e6cf0c8f8cb3c4e8545dd4
hl3470 <hl3470@naver.com>
2024-07-15 17:42:55 +0900

    refactor: todoInput 변경
    todoInput에 텍스를 state로 저장하게 하였다.

--- react-todolist/src/modules/todoInput/todoInput.jsx
+++ react-todolist/src/modules/todoInput/todoInput.jsx
@@ -1,10 +1,23 @@
+import {useState} from "react";
 
 
-export default function TodoInput({onClick}) {
+export default function TodoInput({addTodo}) {
+    const [text, setText] = useState();
+
+    function handleTextChange(e) {
+        console.log(e.target.value);
+        setText(e.target.value);
+    }
+
+    const handleText = () => {
+        addTodo(text);
+    }
+
     return (
+
         <section>
-            <input id="toDoText" type="text" autoFocus placeholder="할 일을 입력해주세요."/>
-            <button onClick={onClick} type="button">확인</button>
+            <input id="toDoText" type="text" autoFocus placeholder="할 일을 입력해주세요." onChange={handleTextChange}/>
+            <button onClick={() => handleText()} type="button">확인</button>
         </section>
     );
 };
